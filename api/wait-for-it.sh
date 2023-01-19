9de43195cf57c0b2513174f9e246e885c16bdbab
Noudea <oudea.nicolas@gmail.com>
2023-01-19 10:43:28 +0100

    :wrench: update wait-for-it script to wait for mongodb

--- api/wait-for-it.sh
+++ api/wait-for-it.sh
@@ -8,3 +8,9 @@ while ! nc -z ampq_rabbitmq 5672; do
   echo 'waiting for rabbitmq container...'
   sleep 1
 done
+
+# wait mongodb service
+while ! nc -z ampq_mongodb 27017; do
+  echo 'waiting for mongodb container...'
+  sleep 1
+done
